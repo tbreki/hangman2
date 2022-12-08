d0284ebd4211d186114413aae5e755ad3513ceb8
tbreki <93223461+tbreki@users.noreply.github.com>
2022-12-08 16:25:15 +0000

    Update words.dart

--- lib/words.dart
+++ lib/words.dart
@@ -1,5 +1,5 @@
 words() {
-  var list = ["vanilla", "chocolate", "strawberry", "caramel"];
+  var list = ["vanilla", "chocolate", "strawberry", "caramel", "banana"];
   var randomItem = (list..shuffle()).first;
   return randomItem;
 }
