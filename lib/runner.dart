f485943aedfd693716ade365340d12be8f1ca8e1
tbreki <93223461+tbreki@users.noreply.github.com>
2023-01-06 12:55:10 +0000

    $$

--- lib/runner.dart
+++ lib/runner.dart
@@ -1,12 +1,106 @@
 import 'package:flutter/material.dart';
-import 'package:hangman/words.dart';
+import 'dart:async';
 
 RunHangman() {
   runApp(MaterialApp(
       home: Scaffold(
-    backgroundColor: Colors.orangeAccent,
     body: Runner(),
   )));
+}
+
+class HangmanGame {
+  static const int hanged = 7;
+
+  List<String> wordlist = [
+    "vanilla",
+    "chocolate",
+    "strawberry",
+    "caramel",
+    "banana"
+  ];
+
+  List<String> usedLetters = [];
+  List<String> _wordToGuess = [" "];
+  int _wrongGuesses = 0;
+
+  StreamController<Null> _onWin = new StreamController<Null>.broadcast();
+  Stream<Null> get onWin => _onWin.stream;
+
+  StreamController<Null> _onLose = new StreamController<Null>.broadcast();
+  Stream<Null> get onLose => _onLose.stream;
+
+  StreamController<int> _onWrong = new StreamController<int>.broadcast();
+  Stream<int> get onWrong => _onWrong.stream;
+
+  StreamController<String> _onRight = new StreamController<String>.broadcast();
+  Stream<String> get onRight => _onRight.stream;
+
+  StreamController<String> _onChange = new StreamController<String>.broadcast();
+  Stream<String> get onChange => _onChange.stream;
+
+  String wordForDisplay = "";
+
+  //HangmanGame(List<String> words) : wordList = new List<String>.from(words);
+  void newGame() {
+    // shuffle the word list into a random order
+    wordlist.shuffle();
+
+    // break the first word from the shuffled list into a list of letters
+    _wordToGuess = wordlist.first.split('');
+
+    // reset the wrong guess count
+    _wrongGuesses = 0;
+
+    // clear the set of guessed letters
+    usedLetters.clear();
+
+    // declare the change (new word)
+    _onChange.add(wordForDisplay);
+  }
+
+  void guessLetter(String letter) {
+    // store guessed letter
+    usedLetters.add(letter);
+
+    // if the guessed letter is present in the word, check for a win
+    // otherwise, check for player death
+    if (_wordToGuess.contains(letter)) {
+      _onRight.add(letter);
+
+      // if (isWordComplete) {
+      //  _onChange.add(fullWord);
+      //  _onWin.add(null);
+    } else {
+      _onChange.add(wordForDisplay);
+
+      //else {
+      _wrongGuesses++;
+
+      _onWrong.add(_wrongGuesses);
+    }
+
+    //if (_wrongGuesses == hanged) {
+    //  _onChange.add(fullWord);
+    //  _onLose.add(null);
+  }
+
+  int get wrongGuesses => _wrongGuesses;
+  List<String> get wordToGuess => _wordToGuess;
+  String get fullWord => wordToGuess.join();
+
+  // String get wordForDisplay => wordToGuess.map((String letter) =>
+  // lettersGuessed.contains(letter) ? letter : "_").join();
+
+  // check to see if every letter in the word has been guessed
+  bool get isWordComplete {
+    for (String letter in _wordToGuess) {
+      if (!usedLetters.contains(letter)) {
+        return false;
+      }
+    }
+
+    return true;
+  }
 }
 
 class Runner extends StatefulWidget {
@@ -17,24 +111,30 @@ class Runner extends StatefulWidget {
 }
 
 class _RunnerState extends State<Runner> {
+  get wordForDisplay => null;
+
+  get wrongGuesses => null;
+
+  String? get usedLetters => null;
+
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         Padding(
           padding: const EdgeInsets.all(30.0),
-          child: Text(
-            words(),
-            style: TextStyle(
-              fontSize: 50,
-              color: Colors.black54,
-            ),
-          ),
+          child: Text(wordForDisplay,
+              style: TextStyle(
+                fontSize: 50,
+                color: Colors.black54,
+              )),
         ),
-        Image.asset("images/hangman1.png"),
+        Image.asset("images/hangman$wrongGuesses.png"),
         Container(
           child: Center(),
-        )
+        ),
+        // todo input keybord to input String!
+        Text(usedLetters!),
       ],
     );
   }
--- lib/words.dart
+++ lib/words.dart
@@ -1,8 +1,1 @@
 import 'dart:math';
-
-words() {
-  var list = ["vanilla", "chocolate", "strawberry", "caramel", "banana"];
-
-  var randomItem = (list..shuffle()).first;
-  return randomItem;
-}
